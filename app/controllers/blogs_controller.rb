9ef8469bef28fc5650755e1a6e667916932e2188
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-02-26 17:48:41 +0900

    add blogs controller and views

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to blogs_path
+  end
+
+  private
+
+  # 以下のコードを追加
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+end
