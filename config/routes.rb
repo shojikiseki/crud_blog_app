d2cfc69e2ce93639a62dfb26e6e937aa53a3fbf5
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-02-27 22:07:38 +0900

    add routes show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/blogs' => 'blogs#index'
   get '/blogs/new' => 'blogs#new'
   post '/blogs' => 'blogs#create'
+  # 以下のコードを追加
+  get '/blogs/:id' => 'blogs#show'
   delete '/blogs/:id' => 'blogs#destroy'
 end
