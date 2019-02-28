0a9d841116a343f32d6a109a30b4a157f3ba0a59
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-02-28 11:06:59 +0900

    fix crud

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.all
@@ -10,34 +11,33 @@ class BlogsController < ApplicationController
 
   def create
     Blog.create(blog_params)
-    redirect_to '/blogs'
+    redirect_to blogs_path
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
-  # 以下のコードを追加
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    blog = Blog.find(params[:id])
-    blog.update(blog_params)
-    redirect_to '/blogs'
+    @blog.update(blog_params)
+    redirect_to blogs_path
   end
 
   def destroy
-    blog = Blog.find(params[:id])
-    blog.destroy
-    redirect_to '/blogs'
+    @blog.destroy
+    redirect_to blogs_path
   end
 
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
 
-  get '/blogs' => 'blogs#index'
-  get '/blogs/new' => 'blogs#new'
-  post '/blogs' => 'blogs#create'
-  get '/blogs/:id' => 'blogs#show'
-  # 以下のコードを追加
-  get '/blogs/:id/edit' => 'blogs#edit'
-  patch '/blogs/:id' => 'blogs#update'
-  delete '/blogs/:id' => 'blogs#destroy'
+  resources :blogs
 end
